95766200d84dc3c0dba7f5be35b63dc77a5bc99c
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-10-08 14:06:15 +0100

    feching data 2

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -21,57 +21,14 @@ function App() {
   }, [dispatch]);
 
   useEffect(() => {
-    // const sendCartData = async () => {
-    // dispatch(
-    //   uiActions.showNotification({
-    //     status: "pending",
-    //     title: "Sending ...",
-    //     message: "Sending cart data!",
-    //   })
-    // );
-    // const response = await fetch(
-    //   "https://react-http-b0e16-default-rtdb.firebaseio.com/cart.json",
-    //   {
-    //     method: "PUT",
-    //     body: JSON.stringify(cart),
-    //   }
-    // );
-    // if (!response.ok) {
-    //   throw new Error("Sending cart data failed.");
-    // }
-    // if (!response.ok) {
-    //   dispatch(
-    //     uiActions.showNotification({
-    //       status: "error",
-    //       title: "Error!",
-    //       message: "Sending cart data failed!",
-    //     })
-    //   );
-    // }
-    // dispatch(
-    //   uiActions.showNotification({
-    //     status: "success",
-    //     title: "Success",
-    //     message: "Sent cart data successfully!",
-    //   })
-    // );
-    // };
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    dispatch(sendCartData(cart));
-
-    // sendCartData().catch((error) => {
-    // dispatch(
-    //   uiActions.showNotification({
-    //     status: "error",
-    //     title: "Error!",
-    //     message: "Sending cart data failed!",
-    //   })
-    // );
-    // });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
